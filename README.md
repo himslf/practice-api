e1a2cc7a43b750838b164022f564ab61b5a0f193
Jonathanh7 <Jonathanh949@gmail.com>
2020-11-23 16:56:54 -0500

    edited

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # practice-api
+
+- Follow along as this lesson's objective is to further get you more practice with api's 
---
+++ script.js
@@ -0,0 +1,5 @@
+// Axios calls will need to be formatted like this:
+// axios.get("your endpoint url",
+//     {
+//       "x-api-key": "your API Key
+//     })
